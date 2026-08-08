31a2d78dbf281aadde0a3b6c607403bb023a78df
gastj01 <gastj01@gmail.com>
2026-08-08 20:05:00 +0200

    fix: use expo-file-system/legacy for uploadAsync

--- app/trade/edit/[id].tsx
+++ app/trade/edit/[id].tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRouter, useLocalSearchParams } from 'expo-router'
 import { Feather } from '@expo/vector-icons'
 import * as DocumentPicker from 'expo-document-picker'
-import * as FileSystem from 'expo-file-system'
+import * as FileSystem from 'expo-file-system/legacy'
 import { supabase } from '../../../src/lib/supabase'
 import type { Trade, TagDefinition } from '../../../src/types'
 
